63abc71b72a16fb194a874d8407b18c1db315c33
hamumeron <nekosuke.1012@outlook.jp>
2025-06-22 09:40:14 +0900

    Update vite.config.js

--- src/client/vite.config.js
+++ src/client/vite.config.js
@@ -1,11 +1,11 @@
+// src/client/vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3000'
-    }
+  build: {
+    outDir: '../dist', // サーバー側が読めるように調整（重要！）
+    emptyOutDir: true
   }
 });
